382e6a727fb8899f41d84600fe3670e2b97e2385
evanma92 <evanma@EvannoMacBook-Pro.local>
2017-10-16 11:38:18 +0800

    created templates folder

--- diversityexplorer/settings.py
+++ diversityexplorer/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'diversityexplorer.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['<workspace>/diversityexplorer/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
