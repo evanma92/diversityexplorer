a3048511a5aa5f04d79102ab843904b5cd453b27
evanma92 <evanma@EvannoMacBook-Pro.local>
2017-10-16 12:08:17 +0800

    Created basic template for index.html and rendered it

--- divexplorer/views.py
+++ divexplorer/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 """
 - Each view exists as a series of individual functions.
@@ -11,8 +12,9 @@ from django.http import HttpResponse
 """
 
 def index(request):
-    return HttpResponse("Diversity Explorer is up and running! "
-                        "<br><a href='/divexplorer/about'>About</a></br>")
+    context_dict = {'boldmessage': "Working with Maurice and Michiel on the best capstone ever"}
+
+    return render(request, 'divexplorer/index.html', context=context_dict)
 
 def about(request):
     return HttpResponse("My CaPsT0n3 is w0nd3rfulz"
