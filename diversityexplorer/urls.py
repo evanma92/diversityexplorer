471da52937bc2426b0b7e7408f3e6614842009e6
evanma92 <evanma@EvannoMacBook-Pro.local>
2017-10-25 13:39:14 +0800

    Directed logout button to logout page

--- diversityexplorer/urls.py
+++ diversityexplorer/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     url(r'^divexplorer/', include('divexplorer.urls')),
 
     url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^signup/$', views.signup, name='signup')
 
 ]
