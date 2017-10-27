a900b05653ed85dbe521e3d31cce06a1f34acabe
evanma92 <evanma@EvannoMacBook-Pro.local>
2017-10-27 09:02:40 +0800

    Did not need to extend user model in django

--- divexplorer/models.py
+++ divexplorer/models.py
@@ -3,26 +3,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.conf import settings
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 # Create your models here.
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=100, null=False)
-    last_name = models.CharField(max_length=256, null=False)
-
-""" Define signals so our Profile model will be updated when we create/update the User instances"""
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
 
 
 class Simulation(models.Model):
