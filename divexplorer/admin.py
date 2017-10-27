8898101dc2084c5b566f87a22572f51a3b6177f0
evanma92 <evanma@EvannoMacBook-Pro.local>
2017-10-27 09:05:33 +0800

    Removed custom user class from admin.py and used django default user class

--- divexplorer/admin.py
+++ divexplorer/admin.py
@@ -2,9 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from divexplorer.models import User, Simulation, Parameter
+from divexplorer.models import Simulation, Parameter
 
 # Register your models here.
-admin.site.register(User)
 admin.site.register(Simulation)
 admin.site.register(Parameter)
